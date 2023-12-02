7185e48cc48a47685b9313580dbce40dd682d1d0
Bakate <b.ba@startupflow.io>
2023-12-02 16:17:59 +0100

    feat(auth): handle user signup

--- actions/auth/signup.actions.ts
+++ actions/auth/signup.actions.ts
@@ -1,26 +1,17 @@
 "use server";
 
 import { getPasswordHash, getSessionExpirationDate } from "@/lib/auth.server";
-<<<<<<< HEAD
-=======
+
 import { createUserSession, getCurrentSession } from "@/lib/manage-session";
->>>>>>> fdaf082 (feat(auth): setup the project with all configs and helpers)
+
 import prisma from "@/lib/prismadb";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 import { SignUpFormSchema } from "@/lib/user-validation";
 import { parse } from "@conform-to/zod";
 import { User } from "@prisma/client";
-<<<<<<< HEAD
 import { z } from "zod";
-
-// TODO create an asynchronous requireOnboardingEmail function
-// 1. get the email from the helper function getCurrentSession using the onboardingEmailSessionKey
-// 2. if there is no email, redirect the user to the "/signup" page
-// 3. return the email
-=======
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import { z } from "zod";
-import { onboardingEmailSessionKey, sessionKey } from "./constants";
+import { onboardingEmailSessionKey } from "./constants";
 
 const requireOnboardingEmail = async () => {
   const email = getCurrentSession(onboardingEmailSessionKey);
@@ -31,7 +22,6 @@ const requireOnboardingEmail = async () => {
 
   return email;
 };
->>>>>>> fdaf082 (feat(auth): setup the project with all configs and helpers)
 
 const signupUser = async ({
   email,
@@ -39,21 +29,14 @@ const signupUser = async ({
   password,
   name,
 }: {
-<<<<<<< HEAD
   email: User["email"]; // I like to communicate to ley you know where the type is coming from. We can also just use "email: string"
-=======
-  email: User["email"];
->>>>>>> fdaf082 (feat(auth): setup the project with all configs and helpers)
   username: User["username"];
   name: User["name"];
   password: string;
 }) => {
   const hashedPassword = await getPasswordHash(password);
-<<<<<<< HEAD
-  // wih prisma, you can create a user and a session in one query. That's awesome!
-=======
 
->>>>>>> fdaf082 (feat(auth): setup the project with all configs and helpers)
+  // wih prisma, you can create a user and a session in one query. That's awesome!
   return prisma.session.create({
     data: {
       expirationDate: getSessionExpirationDate(),
@@ -78,7 +61,7 @@ const signupUser = async ({
 };
 
 export const signupAction = async (formData: FormData) => {
-  const email = ""; // TODO get the email from the requireOnboardingEmail function
+  const email = await requireOnboardingEmail();
 
   const submission = await parse(formData, {
     schema: SignUpFormSchema.superRefine(async (data, ctx) => {
